10f3930b34d023bae1d8b0ec7570ff06c1fafa6e
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-04 18:30:11 -0400

    Create ProductView.Designer.cs

---
+++ ProductView/ProductView.Designer.cs
@@ -0,0 +1,121 @@
+namespace CST4708_Project
+{
+    partial class ProductView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.peripheralToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.monitorToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.laptopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.desktopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.homeToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStrip,
+            this.peripheralToolStrip,
+            this.monitorToolStrip,
+            this.laptopToolStrip,
+            this.desktopToolStrip,
+            this.homeToolStrip});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip.Size = new System.Drawing.Size(1166, 28);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // aboutToolStrip
+            // 
+            this.aboutToolStrip.Name = "aboutToolStrip";
+            this.aboutToolStrip.Size = new System.Drawing.Size(84, 24);
+            this.aboutToolStrip.Text = "About Us";
+            // 
+            // peripheralToolStrip
+            // 
+            this.peripheralToolStrip.Name = "peripheralToolStrip";
+            this.peripheralToolStrip.Size = new System.Drawing.Size(95, 24);
+            this.peripheralToolStrip.Text = "Peripherals";
+            // 
+            // monitorToolStrip
+            // 
+            this.monitorToolStrip.Name = "monitorToolStrip";
+            this.monitorToolStrip.Size = new System.Drawing.Size(82, 24);
+            this.monitorToolStrip.Text = "Monitors";
+            // 
+            // laptopToolStrip
+            // 
+            this.laptopToolStrip.Name = "laptopToolStrip";
+            this.laptopToolStrip.Size = new System.Drawing.Size(76, 24);
+            this.laptopToolStrip.Text = "Laptops";
+            // 
+            // desktopToolStrip
+            // 
+            this.desktopToolStrip.Name = "desktopToolStrip";
+            this.desktopToolStrip.Size = new System.Drawing.Size(84, 24);
+            this.desktopToolStrip.Text = "Desktops";
+            // 
+            // homeToolStrip
+            // 
+            this.homeToolStrip.Name = "homeToolStrip";
+            this.homeToolStrip.Size = new System.Drawing.Size(64, 24);
+            this.homeToolStrip.Text = "Home";
+            // 
+            // ProductView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1166, 495);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "ProductView";
+            this.Text = "ProductView";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem aboutToolStrip;
+        private ToolStripMenuItem peripheralToolStrip;
+        private ToolStripMenuItem monitorToolStrip;
+        private ToolStripMenuItem laptopToolStrip;
+        private ToolStripMenuItem desktopToolStrip;
+        private ToolStripMenuItem homeToolStrip;
+    }
+}
