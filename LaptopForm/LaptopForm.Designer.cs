ee22abf69b17b2c46f83626631f375d81c09992d
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-07 15:11:22 -0500

    Update for Picture Box

--- LaptopForm/LaptopForm.Designer.cs
+++ LaptopForm/LaptopForm.Designer.cs
@@ -28,12 +28,109 @@ namespace CST4708_Project
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.peripheralToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.monitorToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.laptopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.desktopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.homeToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.LaptopFormBackground = new System.Windows.Forms.PictureBox();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LaptopFormBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.menuStrip.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStrip,
+            this.peripheralToolStrip,
+            this.monitorToolStrip,
+            this.laptopToolStrip,
+            this.desktopToolStrip,
+            this.homeToolStrip});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip.Size = new System.Drawing.Size(1209, 28);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // aboutToolStrip
+            // 
+            this.aboutToolStrip.Name = "aboutToolStrip";
+            this.aboutToolStrip.Size = new System.Drawing.Size(92, 24);
+            this.aboutToolStrip.Text = "About Us";
+            // 
+            // peripheralToolStrip
+            // 
+            this.peripheralToolStrip.Name = "peripheralToolStrip";
+            this.peripheralToolStrip.Size = new System.Drawing.Size(103, 24);
+            this.peripheralToolStrip.Text = "Peripherals";
+            // 
+            // monitorToolStrip
+            // 
+            this.monitorToolStrip.Name = "monitorToolStrip";
+            this.monitorToolStrip.Size = new System.Drawing.Size(88, 24);
+            this.monitorToolStrip.Text = "Monitors";
+            // 
+            // laptopToolStrip
+            // 
+            this.laptopToolStrip.Name = "laptopToolStrip";
+            this.laptopToolStrip.Size = new System.Drawing.Size(81, 24);
+            this.laptopToolStrip.Text = "Laptops";
+            // 
+            // desktopToolStrip
+            // 
+            this.desktopToolStrip.Name = "desktopToolStrip";
+            this.desktopToolStrip.Size = new System.Drawing.Size(90, 24);
+            this.desktopToolStrip.Text = "Desktops";
+            // 
+            // homeToolStrip
+            // 
+            this.homeToolStrip.Name = "homeToolStrip";
+            this.homeToolStrip.Size = new System.Drawing.Size(67, 24);
+            this.homeToolStrip.Text = "Home";
+            // 
+            // LaptopFormBackground
+            // 
+            this.LaptopFormBackground.Image = global::CST4708_Project.Properties.Resources.formBackground;
+            this.LaptopFormBackground.Location = new System.Drawing.Point(0, 26);
+            this.LaptopFormBackground.Name = "LaptopFormBackground";
+            this.LaptopFormBackground.Size = new System.Drawing.Size(1209, 641);
+            this.LaptopFormBackground.TabIndex = 2;
+            this.LaptopFormBackground.TabStop = false;
+            // 
+            // LaptopForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1209, 662);
+            this.Controls.Add(this.LaptopFormBackground);
+            this.Controls.Add(this.menuStrip);
+            this.Name = "LaptopForm";
             this.Text = "LaptopForm";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LaptopFormBackground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem aboutToolStrip;
+        private ToolStripMenuItem peripheralToolStrip;
+        private ToolStripMenuItem monitorToolStrip;
+        private ToolStripMenuItem laptopToolStrip;
+        private ToolStripMenuItem desktopToolStrip;
+        private ToolStripMenuItem homeToolStrip;
+        private PictureBox LaptopFormBackground;
     }
 }
