0175b935953a858d030d343b8e39a9389f28fdcb
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-07 14:45:16 -0500

    Updated with Paint Event Method

--- LandingForm/LandingForm.cs
+++ LandingForm/LandingForm.cs
@@ -7,14 +7,14 @@ namespace CST4708_Project
             InitializeComponent();
         }
        
-        private void DesktopButton_Click(object sender, EventArgs e) //navigation to Desktop Button form
+        private void DesktopButton_Click(object sender, EventArgs e) //navigation to Desktop Form
         {
             DesktopForm desktops = new DesktopForm();
             desktops.Show();
             Visible = false;
         }
 
-        private void LaptopButton_Click(object sender, EventArgs e)
+        private void LaptopButton_Click(object sender, EventArgs e) //navigation to Laptop Form
         {
             LaptopForm laptops = new LaptopForm();
             laptops.Show();
@@ -22,18 +22,29 @@ namespace CST4708_Project
 
         }
 
-        private void PeripheralButton_Click(object sender, EventArgs e)
+        private void PeripheralButton_Click(object sender, EventArgs e) //navigation to Peripheral Form
         {
             PeripheralForm peripherals = new PeripheralForm();
             peripherals.Show();
             Visible = false;
         }
 
-        private void MonitorButton_Click(object sender, EventArgs e)
+        private void MonitorButton_Click(object sender, EventArgs e) //navigation to Monitor Form
         {
             MonitorForm monitors = new MonitorForm();
             monitors.Show();
             Visible = false;
+        }
+
+        private void LandingForm_Paint(object sender, PaintEventArgs e)  //LandingForm_Paint -> creates our symbol in the form using graphics object
+        {
+            Graphics graphic = e.Graphics;
+            SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+            graphic.FillRectangle(orangeBrush, 30, 50, 120, 90);
+            graphic.FillRectangle(blueBrush, 50, 70, 81, 50);
+
         }
     }
 }
