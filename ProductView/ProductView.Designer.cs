a9bdedaac54e8ce4458d4d09b7ebe20531ce5656
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-12 17:56:37 -0500

    Update ProductView.Designer.cs

--- ProductView/ProductView.Designer.cs
+++ ProductView/ProductView.Designer.cs
@@ -35,11 +35,15 @@ namespace CST4708_Project
             this.laptopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.desktopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.homeToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.ProductViewBackground = new System.Windows.Forms.PictureBox();
             this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductViewBackground)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip
             // 
+            this.menuStrip.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.menuStrip.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aboutToolStrip,
@@ -51,58 +55,70 @@ namespace CST4708_Project
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
             this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.menuStrip.Size = new System.Drawing.Size(1166, 28);
+            this.menuStrip.Size = new System.Drawing.Size(1207, 28);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip";
             // 
             // aboutToolStrip
             // 
             this.aboutToolStrip.Name = "aboutToolStrip";
-            this.aboutToolStrip.Size = new System.Drawing.Size(84, 24);
+            this.aboutToolStrip.Size = new System.Drawing.Size(92, 24);
             this.aboutToolStrip.Text = "About Us";
             // 
             // peripheralToolStrip
             // 
             this.peripheralToolStrip.Name = "peripheralToolStrip";
-            this.peripheralToolStrip.Size = new System.Drawing.Size(95, 24);
+            this.peripheralToolStrip.Size = new System.Drawing.Size(103, 24);
             this.peripheralToolStrip.Text = "Peripherals";
             // 
             // monitorToolStrip
             // 
             this.monitorToolStrip.Name = "monitorToolStrip";
-            this.monitorToolStrip.Size = new System.Drawing.Size(82, 24);
+            this.monitorToolStrip.Size = new System.Drawing.Size(88, 24);
             this.monitorToolStrip.Text = "Monitors";
             // 
             // laptopToolStrip
             // 
             this.laptopToolStrip.Name = "laptopToolStrip";
-            this.laptopToolStrip.Size = new System.Drawing.Size(76, 24);
+            this.laptopToolStrip.Size = new System.Drawing.Size(81, 24);
             this.laptopToolStrip.Text = "Laptops";
             // 
             // desktopToolStrip
             // 
             this.desktopToolStrip.Name = "desktopToolStrip";
-            this.desktopToolStrip.Size = new System.Drawing.Size(84, 24);
+            this.desktopToolStrip.Size = new System.Drawing.Size(90, 24);
             this.desktopToolStrip.Text = "Desktops";
             // 
             // homeToolStrip
             // 
             this.homeToolStrip.Name = "homeToolStrip";
-            this.homeToolStrip.Size = new System.Drawing.Size(64, 24);
+            this.homeToolStrip.Size = new System.Drawing.Size(67, 24);
             this.homeToolStrip.Text = "Home";
+            // 
+            // ProductViewBackground
+            // 
+            this.ProductViewBackground.Image = global::CST4708_Project.Properties.Resources.formBackground;
+            this.ProductViewBackground.Location = new System.Drawing.Point(-1, 24);
+            this.ProductViewBackground.Name = "ProductViewBackground";
+            this.ProductViewBackground.Size = new System.Drawing.Size(1209, 641);
+            this.ProductViewBackground.TabIndex = 8;
+            this.ProductViewBackground.TabStop = false;
             // 
             // ProductView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1166, 495);
+            this.ClientSize = new System.Drawing.Size(1207, 662);
+            this.Controls.Add(this.ProductViewBackground);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
             this.Name = "ProductView";
             this.Text = "ProductView";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ProductView_Paint);
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductViewBackground)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,5 +133,6 @@ namespace CST4708_Project
         private ToolStripMenuItem laptopToolStrip;
         private ToolStripMenuItem desktopToolStrip;
         private ToolStripMenuItem homeToolStrip;
+        private PictureBox ProductViewBackground;
     }
 }
