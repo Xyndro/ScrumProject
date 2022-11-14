f4502cc25cbdf13ce3d69c608019d1d65e5a9b61
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-14 14:51:19 -0500

    Added paint event handler

--- DesktopForm/DesktopForm.Designer.cs
+++ DesktopForm/DesktopForm.Designer.cs
@@ -36,6 +36,7 @@ namespace CST4708_Project
             this.desktopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.homeToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.DesktopFormBackground = new System.Windows.Forms.PictureBox();
+            this.LaptopFormGoBackButton = new System.Windows.Forms.Button();
             this.menuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DesktopFormBackground)).BeginInit();
             this.SuspendLayout();
@@ -105,15 +106,30 @@ namespace CST4708_Project
             this.DesktopFormBackground.TabIndex = 5;
             this.DesktopFormBackground.TabStop = false;
             // 
+            // LaptopFormGoBackButton
+            // 
+            this.LaptopFormGoBackButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LaptopFormGoBackButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.LaptopFormGoBackButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.LaptopFormGoBackButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LaptopFormGoBackButton.Location = new System.Drawing.Point(36, 175);
+            this.LaptopFormGoBackButton.Name = "LaptopFormGoBackButton";
+            this.LaptopFormGoBackButton.Size = new System.Drawing.Size(121, 28);
+            this.LaptopFormGoBackButton.TabIndex = 6;
+            this.LaptopFormGoBackButton.Text = "Go Back";
+            this.LaptopFormGoBackButton.UseVisualStyleBackColor = false;
+            // 
             // DesktopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1209, 662);
+            this.Controls.Add(this.LaptopFormGoBackButton);
             this.Controls.Add(this.DesktopFormBackground);
             this.Controls.Add(this.menuStrip);
             this.Name = "DesktopForm";
             this.Text = "DesktopForm";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.DesktopForm_Paint);
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DesktopFormBackground)).EndInit();
@@ -132,5 +148,6 @@ namespace CST4708_Project
         private ToolStripMenuItem desktopToolStrip;
         private ToolStripMenuItem homeToolStrip;
         private PictureBox DesktopFormBackground;
+        private Button LaptopFormGoBackButton;
     }
 }
