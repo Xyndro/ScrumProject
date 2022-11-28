7ba99b60135228baa71ff804665c4a107fc63157
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-28 17:17:28 -0500

    Update PeripheralForm.cs

--- PeripheralForm/PeripheralForm.cs
+++ PeripheralForm/PeripheralForm.cs
@@ -16,5 +16,58 @@ namespace CST4708_Project
         {
             InitializeComponent();
         }
+
+        private void PeripheralForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphic = e.Graphics;
+            SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+            graphic.FillRectangle(orangeBrush, 20, 20, 90, 80);
+            graphic.FillRectangle(blueBrush, 39, 40, 52, 40);
+        }
+
+        //methods for navigation
+        private void homeToolStrip_Click(object sender, EventArgs e)
+        {
+            LandingForm home = new LandingForm();
+            home.Show();
+            Visible = false;
+        }
+
+        private void desktopToolStrip_Click(object sender, EventArgs e)
+        {
+            DesktopForm desktop = new DesktopForm();
+            desktop.Show();
+            Visible = false;
+        }
+
+        private void laptopToolStrip_Click(object sender, EventArgs e)
+        {
+            LaptopForm laptop = new LaptopForm();
+            laptop.Show();
+            Visible = false;
+        }
+
+        private void monitorToolStrip_Click(object sender, EventArgs e)
+        {
+            MonitorForm monitor = new MonitorForm();
+            monitor.Show();
+            Visible = false;
+        }
+
+        private void peripheralToolStrip_Click(object sender, EventArgs e)
+        {
+            PeripheralForm peripheral = new PeripheralForm();
+            peripheral.Show();
+            Visible = false;
+        }
+
+        private void logInToolStrip_Click(object sender, EventArgs e)
+        {
+            LogInForm logIn = new LogInForm();
+            logIn.Show();
+            Visible = false;
+        }
     }
 }
