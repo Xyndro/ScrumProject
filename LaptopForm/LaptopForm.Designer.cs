9324b30b37280a131f32b040b5ef3ebe06a9c9c4
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-28 17:14:36 -0500

    Update LaptopForm.Designer.cs

--- LaptopForm/LaptopForm.Designer.cs
+++ LaptopForm/LaptopForm.Designer.cs
@@ -36,9 +36,13 @@ namespace CST4708_Project
             this.laptopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.desktopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.homeToolStrip = new System.Windows.Forms.ToolStripMenuItem();
-            this.LaptopFormGoBackButton = new System.Windows.Forms.Button();
+            this.LaptopFormTitleLabel = new System.Windows.Forms.Label();
+            this.LaptopFormDescription = new System.Windows.Forms.Label();
+            this.LaptopProductsFlowLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.cartPhoto = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.LaptopFormBackground)).BeginInit();
             this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cartPhoto)).BeginInit();
             this.SuspendLayout();
             // 
             // LaptopFormBackground
@@ -46,7 +50,7 @@ namespace CST4708_Project
             this.LaptopFormBackground.Image = global::CST4708_Project.Properties.Resources.formBackground;
             this.LaptopFormBackground.Location = new System.Drawing.Point(0, 26);
             this.LaptopFormBackground.Name = "LaptopFormBackground";
-            this.LaptopFormBackground.Size = new System.Drawing.Size(1209, 641);
+            this.LaptopFormBackground.Size = new System.Drawing.Size(1303, 787);
             this.LaptopFormBackground.TabIndex = 2;
             this.LaptopFormBackground.TabStop = false;
             // 
@@ -65,7 +69,7 @@ namespace CST4708_Project
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
             this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.menuStrip.Size = new System.Drawing.Size(1209, 28);
+            this.menuStrip.Size = new System.Drawing.Size(1302, 28);
             this.menuStrip.TabIndex = 3;
             this.menuStrip.Text = "menuStrip";
             // 
@@ -75,57 +79,95 @@ namespace CST4708_Project
             this.logInToolStrip.Name = "logInToolStrip";
             this.logInToolStrip.Size = new System.Drawing.Size(76, 24);
             this.logInToolStrip.Text = "Log In";
+            this.logInToolStrip.Click += new System.EventHandler(this.logInToolStrip_Click);
             // 
             // peripheralToolStrip
             // 
             this.peripheralToolStrip.Name = "peripheralToolStrip";
             this.peripheralToolStrip.Size = new System.Drawing.Size(103, 24);
             this.peripheralToolStrip.Text = "Peripherals";
+            this.peripheralToolStrip.Click += new System.EventHandler(this.peripheralToolStrip_Click);
             // 
             // monitorToolStrip
             // 
             this.monitorToolStrip.Name = "monitorToolStrip";
             this.monitorToolStrip.Size = new System.Drawing.Size(88, 24);
             this.monitorToolStrip.Text = "Monitors";
+            this.monitorToolStrip.Click += new System.EventHandler(this.monitorToolStrip_Click);
             // 
             // laptopToolStrip
             // 
             this.laptopToolStrip.Name = "laptopToolStrip";
             this.laptopToolStrip.Size = new System.Drawing.Size(81, 24);
             this.laptopToolStrip.Text = "Laptops";
+            this.laptopToolStrip.Click += new System.EventHandler(this.laptopToolStrip_Click);
             // 
             // desktopToolStrip
             // 
             this.desktopToolStrip.Name = "desktopToolStrip";
             this.desktopToolStrip.Size = new System.Drawing.Size(90, 24);
             this.desktopToolStrip.Text = "Desktops";
+            this.desktopToolStrip.Click += new System.EventHandler(this.desktopToolStrip_Click);
             // 
             // homeToolStrip
             // 
             this.homeToolStrip.Name = "homeToolStrip";
             this.homeToolStrip.Size = new System.Drawing.Size(67, 24);
             this.homeToolStrip.Text = "Home";
+            this.homeToolStrip.Click += new System.EventHandler(this.homeToolStrip_Click);
             // 
-            // LaptopFormGoBackButton
+            // LaptopFormTitleLabel
             // 
-            this.LaptopFormGoBackButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.LaptopFormGoBackButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.LaptopFormGoBackButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
-            this.LaptopFormGoBackButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.LaptopFormGoBackButton.Location = new System.Drawing.Point(36, 175);
-            this.LaptopFormGoBackButton.Name = "LaptopFormGoBackButton";
-            this.LaptopFormGoBackButton.Size = new System.Drawing.Size(121, 28);
-            this.LaptopFormGoBackButton.TabIndex = 4;
-            this.LaptopFormGoBackButton.Text = "Go Back";
-            this.LaptopFormGoBackButton.UseVisualStyleBackColor = false;
+            this.LaptopFormTitleLabel.AutoSize = true;
+            this.LaptopFormTitleLabel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LaptopFormTitleLabel.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.LaptopFormTitleLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LaptopFormTitleLabel.Location = new System.Drawing.Point(439, 66);
+            this.LaptopFormTitleLabel.Name = "LaptopFormTitleLabel";
+            this.LaptopFormTitleLabel.Size = new System.Drawing.Size(446, 67);
+            this.LaptopFormTitleLabel.TabIndex = 10;
+            this.LaptopFormTitleLabel.Text = "Laptop Selection";
+            // 
+            // LaptopFormDescription
+            // 
+            this.LaptopFormDescription.AutoSize = true;
+            this.LaptopFormDescription.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LaptopFormDescription.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.LaptopFormDescription.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LaptopFormDescription.Location = new System.Drawing.Point(387, 143);
+            this.LaptopFormDescription.Name = "LaptopFormDescription";
+            this.LaptopFormDescription.Size = new System.Drawing.Size(535, 34);
+            this.LaptopFormDescription.TabIndex = 11;
+            this.LaptopFormDescription.Text = "View our inventory of laptop products here!";
+            // 
+            // LaptopProductsFlowLayout
+            // 
+            this.LaptopProductsFlowLayout.AutoScroll = true;
+            this.LaptopProductsFlowLayout.Location = new System.Drawing.Point(41, 210);
+            this.LaptopProductsFlowLayout.Name = "LaptopProductsFlowLayout";
+            this.LaptopProductsFlowLayout.Size = new System.Drawing.Size(1231, 570);
+            this.LaptopProductsFlowLayout.TabIndex = 23;
+            // 
+            // cartPhoto
+            // 
+            this.cartPhoto.Image = global::CST4708_Project.Properties.Resources.cartPhoto;
+            this.cartPhoto.Location = new System.Drawing.Point(1165, 37);
+            this.cartPhoto.Name = "cartPhoto";
+            this.cartPhoto.Size = new System.Drawing.Size(125, 96);
+            this.cartPhoto.TabIndex = 24;
+            this.cartPhoto.TabStop = false;
             // 
             // LaptopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1209, 662);
-            this.Controls.Add(this.LaptopFormGoBackButton);
+            this.ClientSize = new System.Drawing.Size(1302, 812);
+            this.Controls.Add(this.cartPhoto);
+            this.Controls.Add(this.LaptopProductsFlowLayout);
+            this.Controls.Add(this.LaptopFormDescription);
+            this.Controls.Add(this.LaptopFormTitleLabel);
             this.Controls.Add(this.menuStrip);
             this.Controls.Add(this.LaptopFormBackground);
             this.Name = "LaptopForm";
@@ -134,6 +176,7 @@ namespace CST4708_Project
             ((System.ComponentModel.ISupportInitialize)(this.LaptopFormBackground)).EndInit();
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cartPhoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,6 +191,9 @@ namespace CST4708_Project
         private ToolStripMenuItem laptopToolStrip;
         private ToolStripMenuItem desktopToolStrip;
         private ToolStripMenuItem homeToolStrip;
-        private Button LaptopFormGoBackButton;
+        private Label LaptopFormTitleLabel;
+        private Label LaptopFormDescription;
+        private FlowLayoutPanel LaptopProductsFlowLayout;
+        private PictureBox cartPhoto;
     }
 }
