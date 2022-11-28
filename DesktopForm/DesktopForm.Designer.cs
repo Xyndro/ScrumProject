71d5c6426224391bc889af577bb6c792c0444a89
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-11-28 17:13:21 -0500

    Accompany new Flow Layout Design and Cart Photo

--- DesktopForm/DesktopForm.Designer.cs
+++ DesktopForm/DesktopForm.Designer.cs
@@ -38,8 +38,11 @@ namespace CST4708_Project
             this.DesktopFormBackground = new System.Windows.Forms.PictureBox();
             this.DesktopFormTitleLabel = new System.Windows.Forms.Label();
             this.DesktopFormDescription = new System.Windows.Forms.Label();
+            this.DesktopProductsFlowLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.cartPhoto = new System.Windows.Forms.PictureBox();
             this.menuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DesktopFormBackground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cartPhoto)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -57,7 +60,7 @@ namespace CST4708_Project
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
             this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.menuStrip.Size = new System.Drawing.Size(1209, 28);
+            this.menuStrip.Size = new System.Drawing.Size(1302, 28);
             this.menuStrip.TabIndex = 4;
             this.menuStrip.Text = "menuStrip";
             // 
@@ -67,43 +70,49 @@ namespace CST4708_Project
             this.logInToolStrip.Name = "logInToolStrip";
             this.logInToolStrip.Size = new System.Drawing.Size(76, 24);
             this.logInToolStrip.Text = "Log In";
+            this.logInToolStrip.Click += new System.EventHandler(this.logInToolStrip_Click);
             // 
             // peripheralToolStrip
             // 
             this.peripheralToolStrip.Name = "peripheralToolStrip";
             this.peripheralToolStrip.Size = new System.Drawing.Size(103, 24);
             this.peripheralToolStrip.Text = "Peripherals";
+            this.peripheralToolStrip.Click += new System.EventHandler(this.peripheralToolStrip_Click);
             // 
             // monitorToolStrip
             // 
             this.monitorToolStrip.Name = "monitorToolStrip";
             this.monitorToolStrip.Size = new System.Drawing.Size(88, 24);
             this.monitorToolStrip.Text = "Monitors";
+            this.monitorToolStrip.Click += new System.EventHandler(this.monitorToolStrip_Click);
             // 
             // laptopToolStrip
             // 
             this.laptopToolStrip.Name = "laptopToolStrip";
             this.laptopToolStrip.Size = new System.Drawing.Size(81, 24);
             this.laptopToolStrip.Text = "Laptops";
+            this.laptopToolStrip.Click += new System.EventHandler(this.laptopToolStrip_Click);
             // 
             // desktopToolStrip
             // 
             this.desktopToolStrip.Name = "desktopToolStrip";
             this.desktopToolStrip.Size = new System.Drawing.Size(90, 24);
             this.desktopToolStrip.Text = "Desktops";
+            this.desktopToolStrip.Click += new System.EventHandler(this.desktopToolStrip_Click);
             // 
             // homeToolStrip
             // 
             this.homeToolStrip.Name = "homeToolStrip";
             this.homeToolStrip.Size = new System.Drawing.Size(67, 24);
             this.homeToolStrip.Text = "Home";
+            this.homeToolStrip.Click += new System.EventHandler(this.homeToolStrip_Click);
             // 
             // DesktopFormBackground
             // 
             this.DesktopFormBackground.Image = global::CST4708_Project.Properties.Resources.formBackground;
             this.DesktopFormBackground.Location = new System.Drawing.Point(0, 25);
             this.DesktopFormBackground.Name = "DesktopFormBackground";
-            this.DesktopFormBackground.Size = new System.Drawing.Size(1209, 642);
+            this.DesktopFormBackground.Size = new System.Drawing.Size(1302, 790);
             this.DesktopFormBackground.TabIndex = 5;
             this.DesktopFormBackground.TabStop = false;
             // 
@@ -114,7 +123,7 @@ namespace CST4708_Project
             this.DesktopFormTitleLabel.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
             this.DesktopFormTitleLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.DesktopFormTitleLabel.Location = new System.Drawing.Point(344, 95);
+            this.DesktopFormTitleLabel.Location = new System.Drawing.Point(438, 76);
             this.DesktopFormTitleLabel.Name = "DesktopFormTitleLabel";
             this.DesktopFormTitleLabel.Size = new System.Drawing.Size(472, 67);
             this.DesktopFormTitleLabel.TabIndex = 9;
@@ -126,17 +135,36 @@ namespace CST4708_Project
             this.DesktopFormDescription.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.DesktopFormDescription.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
             this.DesktopFormDescription.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.DesktopFormDescription.Location = new System.Drawing.Point(303, 197);
+            this.DesktopFormDescription.Location = new System.Drawing.Point(391, 154);
             this.DesktopFormDescription.Name = "DesktopFormDescription";
             this.DesktopFormDescription.Size = new System.Drawing.Size(553, 34);
             this.DesktopFormDescription.TabIndex = 10;
             this.DesktopFormDescription.Text = "View our inventory of desktop products here!";
             // 
+            // DesktopProductsFlowLayout
+            // 
+            this.DesktopProductsFlowLayout.AutoScroll = true;
+            this.DesktopProductsFlowLayout.Location = new System.Drawing.Point(36, 211);
+            this.DesktopProductsFlowLayout.Name = "DesktopProductsFlowLayout";
+            this.DesktopProductsFlowLayout.Size = new System.Drawing.Size(1231, 570);
+            this.DesktopProductsFlowLayout.TabIndex = 24;
+            // 
+            // cartPhoto
+            // 
+            this.cartPhoto.Image = global::CST4708_Project.Properties.Resources.cartPhoto;
+            this.cartPhoto.Location = new System.Drawing.Point(1165, 47);
+            this.cartPhoto.Name = "cartPhoto";
+            this.cartPhoto.Size = new System.Drawing.Size(125, 96);
+            this.cartPhoto.TabIndex = 25;
+            this.cartPhoto.TabStop = false;
+            // 
             // DesktopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1209, 662);
+            this.ClientSize = new System.Drawing.Size(1302, 812);
+            this.Controls.Add(this.cartPhoto);
+            this.Controls.Add(this.DesktopProductsFlowLayout);
             this.Controls.Add(this.DesktopFormDescription);
             this.Controls.Add(this.DesktopFormTitleLabel);
             this.Controls.Add(this.DesktopFormBackground);
@@ -148,6 +176,7 @@ namespace CST4708_Project
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DesktopFormBackground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cartPhoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,5 +194,7 @@ namespace CST4708_Project
         private PictureBox DesktopFormBackground;
         private Label DesktopFormTitleLabel;
         private Label DesktopFormDescription;
+        private FlowLayoutPanel DesktopProductsFlowLayout;
+        private PictureBox cartPhoto;
     }
 }
