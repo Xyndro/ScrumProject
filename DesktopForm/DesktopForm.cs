9863a6fda48b19e650b48f0d8a197c160c8f26eb
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-12-02 17:21:32 -0500

    Changes to accompany DataGridView data

--- DesktopForm/DesktopForm.cs
+++ DesktopForm/DesktopForm.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace CST4708_Project
 {
     public partial class DesktopForm : Form
     {
+        SqlConnection myConn;
+        SqlCommand myCmd;
+        SqlDataReader myDataReader;
+
+
         public DesktopForm()
         {
             InitializeComponent();
@@ -31,6 +37,35 @@ namespace CST4708_Project
         {
             DesktopFormTitleLabel.Parent = DesktopFormBackground;
             DesktopFormTitleLabel.BackColor = Color.Transparent;
+
+            fillDesktopDataGridView();
+
+        }
+
+        public void fillDesktopDataGridView()
+        {
+            DesktopDataGridView.Rows.Clear();
+            myConn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\xyndr\\source\\repos\\CST4708_Project\\CST4708_Database.mdf;Integrated Security=True;Connect Timeout=30");
+            myConn.Open();
+
+            myCmd = new SqlCommand("Select * from Desktop", myConn);
+            myDataReader = myCmd.ExecuteReader();
+
+            while (myDataReader.Read())
+            {
+                DesktopDataGridView.Rows.Add(myDataReader["DesktopName"].ToString(), myDataReader["DesktopManufacturer"].ToString(),
+                                             myDataReader["DesktopPrice"].ToString(), myDataReader["DesktopImage"]);
+            }
+
+            myDataReader.Close();
+            myConn.Close();
+
+            //stretches dataGridView for spacing between one item to the next
+            for (int i = 0; i < DesktopDataGridView.Rows.Count; i++)
+            {
+                DataGridViewRow dgvr = DesktopDataGridView.Rows[i];
+                dgvr.Height = 50;
+            }
         }
 
         //methods for navigation
@@ -73,6 +108,13 @@ namespace CST4708_Project
         {
             LogInForm logIn = new LogInForm();
             logIn.Show();
+            Visible = false;
+        }
+
+        private void cartPhoto_Click(object sender, EventArgs e)
+        {
+            CartView cart = new CartView();
+            cart.Show();
             Visible = false;
         }
     }
