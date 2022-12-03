53ef8077908f412df0307b29968df5dbfbcc2b39
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-12-03 17:10:42 -0500

    Update LaptopForm.cs

--- LaptopForm/LaptopForm.cs
+++ LaptopForm/LaptopForm.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,48 @@ namespace CST4708_Project
 {
     public partial class LaptopForm : Form
     {
+        SqlConnection myConn;
+        SqlCommand myCmd;
+        SqlDataReader myDataReader;
+
+
         public LaptopForm()
         {
             InitializeComponent();
+        }
+
+        private void LaptopForm_Load(object sender, EventArgs e)
+        {
+            LaptopFormTitleLabel.Parent = LaptopFormBackground;
+            LaptopFormTitleLabel.BackColor = Color.Transparent;
+
+            fillLaptopDataGridView();
+        }
+
+        public void fillLaptopDataGridView()
+        {
+            LaptopDataGridView.Rows.Clear();
+            myConn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\xyndr\\source\\repos\\CST4708_Project\\CST4708_Database.mdf;Integrated Security=True;Connect Timeout=30");
+            myConn.Open();
+
+            myCmd = new SqlCommand("Select * from Laptop", myConn);
+            myDataReader = myCmd.ExecuteReader();
+
+            while (myDataReader.Read())
+            {
+                LaptopDataGridView.Rows.Add(myDataReader["LaptopName"].ToString(), myDataReader["LaptopManufacturer"].ToString(),
+                                             myDataReader["LaptopPrice"].ToString(), myDataReader["LaptopImage"]);
+            }
+
+            myDataReader.Close();
+            myConn.Close();
+
+            //stretches dataGridView for spacing between one item to the next
+            for (int i = 0; i < LaptopDataGridView.Rows.Count; i++)
+            {
+                DataGridViewRow dgvr = LaptopDataGridView.Rows[i];
+                dgvr.Height = 50;
+            }
         }
 
         private void LaptopForm_Paint(object sender, PaintEventArgs e)
@@ -25,6 +65,56 @@ namespace CST4708_Project
 
             graphic.FillRectangle(orangeBrush, 20, 20, 90, 80);
             graphic.FillRectangle(blueBrush, 39, 40, 52, 40);
+        }
+
+        //methods for navigation
+        private void homeToolStrip_Click(object sender, EventArgs e)
+        {
+            LandingForm home = new LandingForm();
+            home.Show();
+            Visible = false;
+        }
+
+        private void desktopToolStrip_Click(object sender, EventArgs e)
+        {
+            DesktopForm desktop = new DesktopForm();
+            desktop.Show();
+            Visible = false;
+        }
+
+        private void laptopToolStrip_Click(object sender, EventArgs e)
+        {
+            LaptopForm laptop = new LaptopForm();
+            laptop.Show();
+            Visible = false;
+        }
+
+        private void monitorToolStrip_Click(object sender, EventArgs e)
+        {
+            MonitorForm monitor = new MonitorForm();
+            monitor.Show();
+            Visible = false;
+        }
+
+        private void peripheralToolStrip_Click(object sender, EventArgs e)
+        {
+            PeripheralForm peripheral = new PeripheralForm();
+            peripheral.Show();
+            Visible = false;
+        }
+
+        private void logInToolStrip_Click(object sender, EventArgs e)
+        {
+            LogInForm logIn = new LogInForm();
+            logIn.Show();
+            Visible = false;
+        }
+
+        private void cartPhoto_Click(object sender, EventArgs e)
+        {
+            CartView cart = new CartView();
+            cart.Show();
+            Visible = false;
         }
     }
 }
