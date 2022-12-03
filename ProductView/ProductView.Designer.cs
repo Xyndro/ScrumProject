22838624a9377dcdcd2364da4187f200784caabf
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-12-03 17:15:04 -0500

    Update ProductView.Designer.cs

--- ProductView/ProductView.Designer.cs
+++ ProductView/ProductView.Designer.cs
@@ -38,10 +38,20 @@ namespace CST4708_Project
             this.desktopToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.homeToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.cartPhoto = new System.Windows.Forms.PictureBox();
-            this.ProductDetailsPanel = new System.Windows.Forms.Panel();
+            this.ProductViewPicture = new System.Windows.Forms.PictureBox();
+            this.ProductViewName = new System.Windows.Forms.Label();
+            this.ProductViewManufacturer = new System.Windows.Forms.Label();
+            this.ProductViewSKU = new System.Windows.Forms.Label();
+            this.ProductViewCPU = new System.Windows.Forms.Label();
+            this.ProductViewStorage = new System.Windows.Forms.Label();
+            this.ProductViewPrice = new System.Windows.Forms.Label();
+            this.ProductViewDescription = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ProductViewAddToCartButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ProductViewBackground)).BeginInit();
             this.menuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.cartPhoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductViewPicture)).BeginInit();
             this.SuspendLayout();
             // 
             // ProductViewBackground
@@ -49,7 +59,7 @@ namespace CST4708_Project
             this.ProductViewBackground.Image = global::CST4708_Project.Properties.Resources.formBackground;
             this.ProductViewBackground.Location = new System.Drawing.Point(-1, 24);
             this.ProductViewBackground.Name = "ProductViewBackground";
-            this.ProductViewBackground.Size = new System.Drawing.Size(1209, 641);
+            this.ProductViewBackground.Size = new System.Drawing.Size(1235, 703);
             this.ProductViewBackground.TabIndex = 8;
             this.ProductViewBackground.TabStop = false;
             // 
@@ -60,7 +70,7 @@ namespace CST4708_Project
             this.ProductViewLabel.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
             this.ProductViewLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ProductViewLabel.Location = new System.Drawing.Point(361, 122);
+            this.ProductViewLabel.Location = new System.Drawing.Point(361, 84);
             this.ProductViewLabel.Name = "ProductViewLabel";
             this.ProductViewLabel.Size = new System.Drawing.Size(529, 67);
             this.ProductViewLabel.TabIndex = 10;
@@ -81,7 +91,7 @@ namespace CST4708_Project
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
             this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.menuStrip.Size = new System.Drawing.Size(1207, 28);
+            this.menuStrip.Size = new System.Drawing.Size(1234, 28);
             this.menuStrip.TabIndex = 11;
             this.menuStrip.Text = "menuStrip";
             // 
@@ -136,21 +146,114 @@ namespace CST4708_Project
             this.cartPhoto.Size = new System.Drawing.Size(125, 96);
             this.cartPhoto.TabIndex = 22;
             this.cartPhoto.TabStop = false;
+            this.cartPhoto.Click += new System.EventHandler(this.cartPhoto_Click);
             // 
-            // ProductDetailsPanel
+            // ProductViewPicture
             // 
-            this.ProductDetailsPanel.Location = new System.Drawing.Point(25, 221);
-            this.ProductDetailsPanel.Name = "ProductDetailsPanel";
-            this.ProductDetailsPanel.Size = new System.Drawing.Size(1159, 332);
-            this.ProductDetailsPanel.TabIndex = 23;
+            this.ProductViewPicture.Location = new System.Drawing.Point(53, 203);
+            this.ProductViewPicture.Name = "ProductViewPicture";
+            this.ProductViewPicture.Size = new System.Drawing.Size(436, 476);
+            this.ProductViewPicture.TabIndex = 23;
+            this.ProductViewPicture.TabStop = false;
+            // 
+            // ProductViewName
+            // 
+            this.ProductViewName.AutoSize = true;
+            this.ProductViewName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewName.Location = new System.Drawing.Point(646, 273);
+            this.ProductViewName.Name = "ProductViewName";
+            this.ProductViewName.Size = new System.Drawing.Size(62, 22);
+            this.ProductViewName.TabIndex = 24;
+            this.ProductViewName.Text = "Name:";
+            // 
+            // ProductViewManufacturer
+            // 
+            this.ProductViewManufacturer.AutoSize = true;
+            this.ProductViewManufacturer.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewManufacturer.Location = new System.Drawing.Point(646, 328);
+            this.ProductViewManufacturer.Name = "ProductViewManufacturer";
+            this.ProductViewManufacturer.Size = new System.Drawing.Size(121, 22);
+            this.ProductViewManufacturer.TabIndex = 25;
+            this.ProductViewManufacturer.Text = "Manufacturer:";
+            // 
+            // ProductViewSKU
+            // 
+            this.ProductViewSKU.AutoSize = true;
+            this.ProductViewSKU.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewSKU.Location = new System.Drawing.Point(646, 219);
+            this.ProductViewSKU.Name = "ProductViewSKU";
+            this.ProductViewSKU.Size = new System.Drawing.Size(122, 22);
+            this.ProductViewSKU.TabIndex = 26;
+            this.ProductViewSKU.Text = "SKU Number:";
+            // 
+            // ProductViewCPU
+            // 
+            this.ProductViewCPU.AutoSize = true;
+            this.ProductViewCPU.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewCPU.Location = new System.Drawing.Point(646, 378);
+            this.ProductViewCPU.Name = "ProductViewCPU";
+            this.ProductViewCPU.Size = new System.Drawing.Size(53, 22);
+            this.ProductViewCPU.TabIndex = 27;
+            this.ProductViewCPU.Text = "CPU:";
+            // 
+            // ProductViewStorage
+            // 
+            this.ProductViewStorage.AutoSize = true;
+            this.ProductViewStorage.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewStorage.Location = new System.Drawing.Point(646, 429);
+            this.ProductViewStorage.Name = "ProductViewStorage";
+            this.ProductViewStorage.Size = new System.Drawing.Size(129, 22);
+            this.ProductViewStorage.TabIndex = 28;
+            this.ProductViewStorage.Text = "Storage Space:";
+            // 
+            // ProductViewPrice
+            // 
+            this.ProductViewPrice.AutoSize = true;
+            this.ProductViewPrice.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewPrice.Location = new System.Drawing.Point(646, 482);
+            this.ProductViewPrice.Name = "ProductViewPrice";
+            this.ProductViewPrice.Size = new System.Drawing.Size(58, 22);
+            this.ProductViewPrice.TabIndex = 29;
+            this.ProductViewPrice.Text = "Price:";
+            // 
+            // ProductViewDescription
+            // 
+            this.ProductViewDescription.AutoSize = true;
+            this.ProductViewDescription.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewDescription.Location = new System.Drawing.Point(646, 535);
+            this.ProductViewDescription.Name = "ProductViewDescription";
+            this.ProductViewDescription.Size = new System.Drawing.Size(109, 22);
+            this.ProductViewDescription.TabIndex = 30;
+            this.ProductViewDescription.Text = "Description:";
+            // 
+            // ProductViewAddToCartButton
+            // 
+            this.ProductViewAddToCartButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ProductViewAddToCartButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ProductViewAddToCartButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.ProductViewAddToCartButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ProductViewAddToCartButton.Location = new System.Drawing.Point(802, 640);
+            this.ProductViewAddToCartButton.Name = "ProductViewAddToCartButton";
+            this.ProductViewAddToCartButton.Size = new System.Drawing.Size(196, 39);
+            this.ProductViewAddToCartButton.TabIndex = 32;
+            this.ProductViewAddToCartButton.Text = "Add To Cart";
+            this.ProductViewAddToCartButton.UseVisualStyleBackColor = false;
             // 
             // ProductView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1207, 662);
-            this.Controls.Add(this.ProductDetailsPanel);
+            this.ClientSize = new System.Drawing.Size(1234, 722);
+            this.Controls.Add(this.ProductViewAddToCartButton);
+            this.Controls.Add(this.ProductViewDescription);
+            this.Controls.Add(this.ProductViewPrice);
+            this.Controls.Add(this.ProductViewStorage);
+            this.Controls.Add(this.ProductViewCPU);
+            this.Controls.Add(this.ProductViewSKU);
+            this.Controls.Add(this.ProductViewManufacturer);
+            this.Controls.Add(this.ProductViewName);
+            this.Controls.Add(this.ProductViewPicture);
             this.Controls.Add(this.cartPhoto);
             this.Controls.Add(this.menuStrip);
             this.Controls.Add(this.ProductViewLabel);
@@ -162,6 +265,7 @@ namespace CST4708_Project
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.cartPhoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductViewPicture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,6 +282,15 @@ namespace CST4708_Project
         private ToolStripMenuItem desktopToolStrip;
         private ToolStripMenuItem homeToolStrip;
         private PictureBox cartPhoto;
-        private Panel ProductDetailsPanel;
+        private PictureBox ProductViewPicture;
+        private Label ProductViewName;
+        private Label ProductViewManufacturer;
+        private Label ProductViewSKU;
+        private Label ProductViewCPU;
+        private Label ProductViewStorage;
+        private Label ProductViewPrice;
+        private Label ProductViewDescription;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button ProductViewAddToCartButton;
     }
 }
