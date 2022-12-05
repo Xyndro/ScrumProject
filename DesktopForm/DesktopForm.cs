e418b6b99faf798bff942af24af87b76c44184c6
Joseph Feliciano <100709990+Xyndro@users.noreply.github.com>
2022-12-05 11:15:54 -0500

    Update DesktopForm.cs

--- DesktopForm/DesktopForm.cs
+++ DesktopForm/DesktopForm.cs
@@ -23,20 +23,16 @@ namespace CST4708_Project
             InitializeComponent();
         }
 
-        private void DesktopForm_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics graphic = e.Graphics;
-            SolidBrush orangeBrush = new SolidBrush(Color.Orange);
-            SolidBrush blueBrush = new SolidBrush(Color.Blue);
-
-            graphic.FillRectangle(orangeBrush, 20, 20, 90, 80);
-            graphic.FillRectangle(blueBrush, 39, 40, 52, 40);
-        }
-
         private void DesktopForm_Load(object sender, EventArgs e)
         {
             DesktopFormTitleLabel.Parent = DesktopFormBackground;
             DesktopFormTitleLabel.BackColor = Color.Transparent;
+
+            DesktopFormDescription.Parent = DesktopFormBackground;
+            DesktopFormDescription.BackColor = Color.Transparent;
+
+            LogInStatus.Parent = DesktopFormBackground;
+            LogInStatus.BackColor = Color.Transparent;
 
             fillDesktopDataGridView();
 
